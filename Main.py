ce6e97ca6044ef8686277e786996ab79b87bbcba
Arnav <arnav.patil115@gmail.com>
2026-04-17 21:01:52 -0400

    Update Main.py

--- Main.py
+++ Main.py
@@ -6,18 +6,18 @@ def ask_user_preferences():
     "Asks users their preferences and stores them in varibles"
     Preferences = {}
 
-    Preferences[genres] = input("What genres are you looking for:")
-    Preferences[date_range] = input("What is your prefered release date range?")
-    prefereed_actors = input("Do you have any prefered actors or actresses?")
+    #Currently stores all user preferences into a dictionary. For the time being, they will remain strings. Genres may be changed to be a list, date_range a tuple, and actors a string. 
+    Preferences["genres"] = input("What genres are you looking for:")
+    Preferences["date_range"] = input("What is your prefered release date range?")
+    Preferences["actors"] = input("Do you have any prefered actors or actresses?")
 
+    return Preferences
 
-    return prefered_genre, prefereed_date_range, prefereed_actors
-
-def display_user_preferences(prefered_genre, prefereed_date_range, prefereed_actors):
+def display_user_preferences(Preferences):
     "Unit test to verify that the user's preferences were correctly stored and are the corerct type"
-    
-    print(f'Genres: {prefered_genre}')
-    print(f'Date Range: {prefereed_date_range}')
-    print(f'Actors/Actresses: {prefereed_actors}')
 
-display_user_preferences(*ask_user_preferences())
+    print(f'Genres: {Preferences["genres"]}')
+    print(f'Date Range: {Preferences["date_range"]}')
+    print(f'Actors/Actresses: {Preferences["actors"]}')
+
+display_user_preferences(ask_user_preferences())
