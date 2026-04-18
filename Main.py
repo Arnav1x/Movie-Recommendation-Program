290b46c46b5e26d566e9669142239720fa50b760
Arnav <arnav.patil115@gmail.com>
2026-04-17 20:55:46 -0400

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,10 +1,21 @@
+
+
+
+
 def ask_user_preferences():
-    prefered_genre = input("What genres are you looking for:")
-    prefereed_date_range = input("What is your prefered release date range?")
+    "Asks users their preferences and stores them in varibles"
+    Preferences = {}
+
+    Preferences[genres] = input("What genres are you looking for:")
+    Preferences[date_range] = input("What is your prefered release date range?")
     prefereed_actors = input("Do you have any prefered actors or actresses?")
+
+
     return prefered_genre, prefereed_date_range, prefereed_actors
 
 def display_user_preferences(prefered_genre, prefereed_date_range, prefereed_actors):
+    "Unit test to verify that the user's preferences were correctly stored and are the corerct type"
+    
     print(f'Genres: {prefered_genre}')
     print(f'Date Range: {prefereed_date_range}')
     print(f'Actors/Actresses: {prefereed_actors}')
