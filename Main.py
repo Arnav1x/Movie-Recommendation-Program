21eb499b557733681d8b0a6a39c2bb18b1cdec7e
Arnav <arnav.patil115@gmail.com>
2026-04-17 22:32:18 -0400

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,9 +1,16 @@
 import csv
+import random
 
-def load_csv():
+def load_movies():
+    """
+    Loads movies from dataset and stores into list of dictionaries
+
+    Returns: movies(list) 
+    
+    """
     movies = []
 
-    with open("movies_dataset_simple.csv", "r") as file:
+    with open("movies_dataset_simple.csv", "r", encoding="cp1252", errors="replace") as file:
         reader = csv.DictReader(file)
 
         for row in reader:
@@ -11,10 +18,10 @@ def load_csv():
 
     return movies
 
-
-
 def ask_user_preferences():
-    "Asks users their preferences and stores them in varibles"
+    """
+    Asks users their preferences and stores them in varibles
+    """
     Preferences = {}
 
     #Currently stores all user preferences into a dictionary. For the time being, they will remain strings, types may be adjusted later. 
@@ -24,11 +31,25 @@ def ask_user_preferences():
 
     return Preferences
 
-def display_user_preferences(Preferences):
-    "Unit test to verify that the user's preferences were correctly stored"
-
+def display_unit_tests(Preferences):
+    """Unit test to verify that the user's preferences were correctly stored"""
+    
     print(f'Genres: {Preferences["genres"]}')
     print(f'Date Range: {Preferences["date_range"]}')
     print(f'Actors/Actresses: {Preferences["actors"]}')
 
-load_csv()
+def test_movies_list(movies):
+
+    print("Total movies loaded:", len(movies))
+
+    print("Keys in a movie entry:")
+    print(list(movies[0].keys()))
+
+    print("Random movie sample:")
+    print(movies[random.randint(1,len(movies))])
+
+
+if __name__ == "__main__":
+    #display_unit_tests(ask_user_preferences())
+    movies = load_movies()
+    test_movies_list(movies)
