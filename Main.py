3cda3c0920e9634f9b21dee2316f326f5688cbcb
Arnav <arnav.patil115@gmail.com>
2026-04-17 21:04:39 -0400

    Update Main.py

--- Main.py
+++ Main.py
@@ -6,15 +6,15 @@ def ask_user_preferences():
     "Asks users their preferences and stores them in varibles"
     Preferences = {}
 
-    #Currently stores all user preferences into a dictionary. For the time being, they will remain strings. Genres may be changed to be a list, date_range a tuple, and actors a string. 
-    Preferences["genres"] = input("What genres are you looking for:")
-    Preferences["date_range"] = input("What is your prefered release date range?")
-    Preferences["actors"] = input("Do you have any prefered actors or actresses?")
+    #Currently stores all user preferences into a dictionary. For the time being, they will remain strings, types may be adjusted later. 
+    Preferences["genres"] = input("What genres are you looking for:") #may be changed to a list using .lower().split(',')
+    Preferences["date_range"] = input("What is your prefered release date range?") #may be changed to a tuple
+    Preferences["actors"] = input("Do you have any prefered actors or actresses?") #may be changed to a list using .lower().split(',')
 
     return Preferences
 
 def display_user_preferences(Preferences):
-    "Unit test to verify that the user's preferences were correctly stored and are the corerct type"
+    "Unit test to verify that the user's preferences were correctly stored"
 
     print(f'Genres: {Preferences["genres"]}')
     print(f'Date Range: {Preferences["date_range"]}')
