9317a97d9c2b281a14e5e55a4fac04ca01379400
Arnav <arnav.patil115@gmail.com>
2026-04-17 20:50:21 -0400

    Update Main.py

--- Main.py
+++ Main.py
@@ -2,10 +2,11 @@ def ask_user_preferences():
     prefered_genre = input("What genres are you looking for:")
     prefereed_date_range = input("What is your prefered release date range?")
     prefereed_actors = input("Do you have any prefered actors or actresses?")
+    return prefered_genre, prefereed_date_range, prefereed_actors
 
-
-def display_user_preferences():
-    print(f'Genres: {prefered_genres}')
+def display_user_preferences(prefered_genre, prefereed_date_range, prefereed_actors):
+    print(f'Genres: {prefered_genre}')
     print(f'Date Range: {prefereed_date_range}')
     print(f'Actors/Actresses: {prefereed_actors}')
 
+display_user_preferences(*ask_user_preferences())
